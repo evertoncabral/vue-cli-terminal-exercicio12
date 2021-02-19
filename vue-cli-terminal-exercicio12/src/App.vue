3ee545b87208052174a1c3d1be63a98ba6b47c9c
Everton Cabral <evertoncab@gmail.com>
2021-02-18 21:48:13 -0300

    usando v-model

--- vue-cli-terminal-exercicio12/src/App.vue
+++ vue-cli-terminal-exercicio12/src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div id="app">
-  
+    <input type="text" @input="text = $event.target.value" :value="text" />
+    {{ text }}
+
+    <br />
+
+    <input type="text2"  v-model="text2" />
+    {{ text2 }}
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      text: "digite aqui -> campo input",
+      text2: "digite aqui -> v-model",
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
